960e84b3bb1f9f5a7117c91700745583032c8b22
jreider2 <julesreider@gmail.com>
2017-11-19 12:27:58 -0600

    testing things with authentication

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var path = require('path');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStategy = require('passport-local').Strategy;
+//the decalrations bellow are needed for google authentication 
+var GoogleAuth = require('google-auth-library');
+var CLIENT_ID = "332330513278-7uboe8hl0v0cdnan6orf83opb3p29j9m.apps.googleusercontent.com";
+var auth = new GoogleAuth;
+var client = new auth.OAuth2(CLIENT_ID, '', '');
 
 // getting-started.js
 var mongoose = require('mongoose');
@@ -16,6 +21,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/views/index.html');
+    
 });
 app.get('/genre', function(req,res){
     res.sendFile(__dirname + '/views/genre.html');
@@ -31,4 +37,46 @@ app.get('/genre/horror', function(req,res) {
 });
 app.listen(server_port, function() {
     console.log("Listening on port : " + server_port);
+});
+
+
+
+//this was a test post route. not important. 
+app.post("/examplePost", function(req, res) {
+    //store new object in req.body
+    //var newNote = req.body;
+    //push new note to JSON
+    //notes["travelNotes"].push(newNote);
+    //return simple JSON object
+    // res.json({
+    //   "message": "post complete to server !!",
+    //   "test": req.body
+    // });
+    res.send("Message") 
+    //msg to terminal 
+    console.log("Routing to examplePost post request reached.")
+});
+
+app.post("/sendToken", function(req, res) {  
+
+    //somehow I need to make a post request to the google server
+    //in order to authenticate 
+
+    //here are my attempts 
+    //(before I realized jquery doesn't work on backend)
+
+    // var tokenInfoEndpointURL = "https://www.googleapis.com/oauth2/v3/tokeninfo?" + req;
+    // $.get( tokenInfoEndpointURL, function( data ) {
+    //     //deal with return JSON from google
+    //     console.log(data);
+    // });
+
+    // $.post("https://www.googleapis.com/oauth2/v3/tokeninfo", {id_token: req}, function (response) {
+    //     console.log("server post response returned..." + response);
+    // });
+
+    console.log("req = " + req.body); //troubleshooting 
+    res.send("R"); //sending a resonse 
+    console.log("Routing to testPost post request reached!");
+    
 });
--- public/scripts/sign.js
+++ public/scripts/sign.js
@@ -2,14 +2,40 @@ console.log("SIGN.JS CONNNECTEDDDDD");
 
 $('h1').on('click', function(){
     console.log("another click");
-
 });
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-}
+//gapi.auth2.init({ client_id: '332330513278-7uboe8hl0v0cdnan6orf83opb3p29j9m.apps.googleusercontent.com'});
 
-$('#signOut').on('click', signOut());
+// function onSignIn(googleUser) {
+//     // Useful data for your client-side scripts:
+
+
+//       // The ID token you need to pass to your backend:
+//       var id_token = googleUser.getAuthResponse().id_token;
+//       console.log("ID Token: " + id_token);
+
+      
+
+//       //professor's example
+//       var note_text = $(".note-input input").val();
+//       var created = new Date();
+//       //create new note
+//       var newNote = {"created":created, "note":note_text};
+//       $.post("examplePost", newNote, function (response) {
+//           console.log("server post response returned..." + response);
+//       });
+// };
+
+
+// function signOut() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//         console.log('User signed out.');
+//     });
+// }
+
+
+// $('.signInDiv').on('click', onSignIn());
+
+// $('#signOut').on('click', signOut());
+
