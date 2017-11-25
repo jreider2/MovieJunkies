ec5603ff72a5a5cd1536ac393bf373c8c1c42de7
jreider2 <julesreider@gmail.com>
2017-11-25 12:10:16 -0600

    refactored signIn. No more token. Sendign ID directly to backend. added http and body-parser.

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var mongoose = require('mongoose');
-var passport = require('passport');
-var LocalStategy = require('passport-local').Strategy;
-//the decalrations bellow are needed for google authentication 
 var GoogleAuth = require('google-auth-library');
 var CLIENT_ID = "332330513278-7uboe8hl0v0cdnan6orf83opb3p29j9m.apps.googleusercontent.com";
 var auth = new GoogleAuth;
 var client = new auth.OAuth2(CLIENT_ID, '', '');
+//var XMLHttpRequest = require('xhr2'); //npm install xhr2
+
+var http = require("http");
+var bodyParser = require("body-parser");
 
 // getting-started.js
 var mongoose = require('mongoose');
@@ -17,11 +18,20 @@ mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
 
 var server_port = 3000;
 
+//set as static file server...
 app.use(express.static(__dirname + '/public'));
+
+//parse jQuery JSON to useful JS object
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//create http server
+http.createServer(app).listen(server_port);
+// app.listen(server_port, function() {
+//     console.log("Listening on port : " + server_port);
+// });
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/views/index.html');
-    
 });
 app.get('/genre', function(req,res){
     res.sendFile(__dirname + '/views/genre.html');
@@ -35,48 +45,30 @@ app.get('/login', function(req,res) {
 app.get('/genre/horror', function(req,res) {
     res.sendFile(__dirname + '/views/horrorPage.html');
 });
-app.listen(server_port, function() {
-    console.log("Listening on port : " + server_port);
+
+//Recieve id from sign in 
+app.post("/userIdPost", function(req, res) {
+    //console.log("Routing to examplePost post request reached.");//msg to terminal 
+    console.log(req.body); //print ID to console. 
+    if (req.body !== undefined){ // check if undefined
+        res.send({ status: "Success" });
+    }
+    else{
+        //send fail response
+        res.send({ status: "failure" });
+    }
+    //res.send({ status: "Success" });
 });
 
-
-
-//this was a test post route. not important. 
-app.post("/examplePost", function(req, res) {
+//testing how to use a post route 
+app.post("/notes", function(req, res) {
     //store new object in req.body
-    //var newNote = req.body;
+    var newNote = req.body;
+    console.log(newNote);
     //push new note to JSON
     //notes["travelNotes"].push(newNote);
     //return simple JSON object
-    // res.json({
-    //   "message": "post complete to server !!",
-    //   "test": req.body
-    // });
-    res.send("Message") 
-    //msg to terminal 
-    console.log("Routing to examplePost post request reached.")
-});
-
-app.post("/sendToken", function(req, res) {  
-
-    //somehow I need to make a post request to the google server
-    //in order to authenticate 
-
-    //here are my attempts 
-    //(before I realized jquery doesn't work on backend)
-
-    // var tokenInfoEndpointURL = "https://www.googleapis.com/oauth2/v3/tokeninfo?" + req;
-    // $.get( tokenInfoEndpointURL, function( data ) {
-    //     //deal with return JSON from google
-    //     console.log(data);
-    // });
-
-    // $.post("https://www.googleapis.com/oauth2/v3/tokeninfo", {id_token: req}, function (response) {
-    //     console.log("server post response returned..." + response);
-    // });
-
-    console.log("req = " + req.body); //troubleshooting 
-    res.send("R"); //sending a resonse 
-    console.log("Routing to testPost post request reached!");
-    
-});
+    res.json({
+      "message": "post complete to server"
+    });
+  });
